e9966d7255a40d7ed63a9f821f58900fdc772541
Xa-C24 <9335@holbertonstudents.com>
2024-12-18 09:25:55 +0100

    add task5 with proxy

---
+++ task5/back-end/api.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)             # Active CORS pour permettre les requêtes du front-end
+
+@app.route('/api/hello')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5252)
